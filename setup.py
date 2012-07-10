e03f95fbeee9f27ce7cb323b5d2b5edf3cd36f15
Vladimir Yakovlev <nvbn.rm@gmail.com>
2012-07-10 17:03:48 +0400

    shit

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '0.1'
 
 setup(name='cantailme-client',
       version=version,
-      description="",
+      description="Client-sender for cantail.me",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -13,7 +13,7 @@ setup(name='cantailme-client',
       author='Vladimir Iakovlev',
       author_email='nvbn.rm@gmail.com',
       url='http://cantail.me/',
-      license='GPLм2+',
+      license='GPLv2+',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
